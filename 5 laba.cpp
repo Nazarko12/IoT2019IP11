77154540fcc2a203cc073bb3dd232451bffad86d
Nazarko12 <57871748+Nazarko12@users.noreply.github.com>
2019-12-03 19:39:32 +0200

    Modified code for laboratory work 5

--- 5 laba.cpp
+++ 5 laba.cpp
@@ -1,115 +1,118 @@
 #include <iostream>
 #include <cmath>
+#define size 5
 using namespace std;
+
 
 class Matrix {
 private:
-    int mtx[5];
+    int matrix[size];
 public:
-    void InputElement(int a) {
-        cin >> mtx[a];
-    }
-    void sMatrix(int a) {
-        static int ptr = 0;
-        mtx[ptr] = a;
-        if (ptr == 4) ptr = -1;
-        ptr++;
-    }
+    friend void outputMatrix(Matrix*, int);
 
-    int *GetMatrix() {
-        return mtx;
+    void inputElement(int arrayElementNum) {
+        cin >> matrix[arrayElementNum];
+    }
+    void showMatrix(int arrayElement) {
+        static int elementIndicator = 0;
+        matrix[elementIndicator] = arrayElement;
+        if (elementIndicator == 4) elementIndicator = -1;
+        elementIndicator++;
     }
 
-        friend void OutputMatrix(Matrix *A, int n);
+    int *getMatrix() {
+        return matrix;
+    }
 
+    void merge(int start, int middle, int end) {
+        int temporaryArray[5];
+        int i = start, j = middle + 1, k = 0;
 
-        void merge(int start, int mid, int end) {
-            int t[5];
-            int i = start, j = mid + 1, k = 0;
-
-            while (i <= mid && j <= end) {
-                if (this->mtx[i] <= this->mtx[j]) {
-                    t[k] = this->mtx[i];
-                    k++;
-                    i++;
-                } else {
-                    t[k] = this->mtx[j];
-                    k++;
-                    j++;
-                }
-            }
-
-            while (i <= mid) {
-                t[k] = this->mtx[i];
+        while (i <= middle && j <= end) {
+            if (this->matrix[i] <= this->matrix[j]) {
+                temporaryArray[k] = this->matrix[i];
                 k++;
                 i++;
-            }
-
-            while (j <= end) {
-                t[k] = this->mtx[j];
+            } else {
+                temporaryArray[k] = this->matrix[j];
                 k++;
                 j++;
             }
-            for (i = start; i <= end;i++) {
-                this->mtx[i] = t[i - start];
-            }
         }
 
-        void mergeSort(int start, int end) {
-
-            if (start < end) {
-                int mid = (start + end) / 2;
-                mergeSort(start, mid);
-                mergeSort(mid + 1, end);
-                merge(start, mid, end);
-            }
-
+        while (i <= middle) {
+            temporaryArray[k] = this->matrix[i];
+            k++;
+            i++;
         }
+
+        while (j <= end) {
+            temporaryArray[k] = this->matrix[j];
+            k++;
+            j++;
+        }
+        for (i = start; i <= end;i++) {
+            this->matrix[i] = temporaryArray[i - start];
+        }
+    }
+
+    void mergeSortByGrowth(int start, int end) {
+        if (start < end) {
+            int middle = (start + end) / 2;
+            mergeSortByGrowth(start, middle);
+            mergeSortByGrowth(middle + 1, end);
+            merge(start, middle, end);
+        }
+    }
 };
 
-void OutputMatrix(Matrix* mtx, int n){
-    for(int i = 0; i<n; ++i)
+void outputMatrix(Matrix* matrixExample, int numElements){
+    for(int i = 0; i < numElements; ++i)
     {
-        cout << mtx -> mtx[i];
+        cout << matrixExample -> matrix[i];
     }
     cout << endl;
 }
 
 
 int main() {
-    Matrix matrix[5];
+    Matrix matrixExample[5];
+
     for (int i = 0; i<5; i++)
         for (int j = 0; j<5;j++){
-            matrix[j].InputElement(i);
+            matrixExample[j].inputElement(i);
         }
-    Matrix matrix1;
-    for (int i = 0; i<5; i++)
-        matrix[i].mergeSort(0, 4);
+    Matrix matrixTemporary;
+    for (int i = 0; i<5; i++) {
+        matrixExample[i].mergeSort(0, 4);
+    }
+
     for (int i = 0; i<5; i++){
         for (int j = 0; j<5; j++) {
-            cout << matrix[j].GetMatrix()[i] << "\t ";
-            matrix1.sMatrix(matrix[j].GetMatrix()[i]);
+            cout << matrixExample[j].getMatrix()[i] << "\t ";
+            matrixTemporary.showMatrix(matrixExample[j].getMatrix()[i]);
         }
         cout << endl;
     }
-    double geom = 0;
-    int f1 = 0, f2 = 0, f3 = 0, f4 = 0;
-        for (int i = 1; i < 5; i++) {                                  // f1
-            f1 += matrix[i].GetMatrix()[0];
+
+    double geometricAverage = 0;
+    int sumDiagonal1 = 0, sumDiagonal2 = 0, sumDiagonal3 = 0, sumDiagonal4 = 0;
+        for (int i = 1; i < 5; i++) {
+            sumDiagonal1 += matrixExample[i].getMatrix()[0];
         }
-        for (int i = 2; i < 5; i++) {                                  // f1
-            f2 += matrix[i].GetMatrix()[1];
+        for (int i = 2; i < 5; i++) {
+            sumDiagonal2 += matrixExample[i].getMatrix()[1];
         }
-        for (int i = 3; i < 5; i++) {                                  // f1
-            f3 += matrix[i].GetMatrix()[2];
+        for (int i = 3; i < 5; i++) {
+            sumDiagonal3 += matrixExample[i].getMatrix()[2];
         }
-        for (int i = 4; i < 5; i++) {                                  // f1
-            f4 += matrix[i].GetMatrix()[3];
+        for (int i = 4; i < 5; i++) {
+            sumDiagonal4 += matrixExample[i].getMatrix()[3];
         }
-    cout<<"\n";
-    geom = pow(f1*f2*f3*f4, 1.0/4.0);
-    cout << "f1(): " << f1 << "\n" << "f2(): " << f2 << "\n" << "f3(): " << f3 << "\n" << "f4(): " << f4 << "\n\n";
-    cout << "F(f()): " << geom << "\n";
+    cout << "\n";
+    geometricAverage = pow(sumDiagonal1*sumDiagonal2*sumDiagonal3*sumDiagonal4, 1.0/4.0);
+    cout << "f1(): " << sumDiagonal1 << "\n" << "f2(): " << sumDiagonal2 << "\n" << "f3(): " << sumDiagonal3 << "\n" << "f4(): " << sumDiagonal4 << "\n\n";
+    cout << "F(f()): " << geometricAverage << "\n";
 
     return 0;
 }
